8ae65a107849d0e4306e1c70e15909db77c1a625
jimlin2004 <jim.lin2004@gmail.com>
2023-09-15 18:38:21 +0800

    將線段樹添加懶標(lazy tag)，以及改成陣列型binary tree(2023/09/15)

--- algorithm/SegmentTree.cpp
+++ algorithm/SegmentTree.cpp
@@ -1,33 +1,66 @@
-struct node{
-    int val;
-    node *l,*r;
-    node(int v=0):val(v){};
-    node(node* l,node* r):l(l),r(r){pull();}
-    void pull(){val=min(l->val,r->val);}
-    //l->val就是(*l).val，注意.的優先順序比*還高
-};
+#define MAXN 1000
+int data[MAXN]; //原數據
+int st[4 * MAXN]; //線段樹
+int tag[4 * MAXN]; //懶標
 
-int v[N];//原數列
-node* build(int l,int r){
-    if(l+1==r) return new node(v[l]);
-    int mid=(l+r)/2;
-    return new node(build(l,mid),build(mid,r));
+inline int pull(int l, int r) {
+// 隨題目改變sum、max、min
+// l、r是左右樹的index
+    return st[l] + st[r];
 }
 
-void modify(node* a,int l,int r,int pos,int k){
-    //把pos位置的值換成k
-    if(l+1==r){a->val=k;return;};
-    int mid=(l+r)/2;
-    if(pos<mid) modify(a->l,l,mid,pos,k);
-    else modify(a->r,mid,r,pos,k);
-    a->pull();
+void build(int l, int r, int i) {
+// 在[l, r]區間建樹，目前根的index為i
+    if (l == r) {
+        st[i] = data[i];
+        return;
+    }
+    int mid = l + ((r - l) >> 1);
+    build(l, mid, i * 2);
+    build(mid + 1, r, i * 2 + 1);
+    st[i] = pull(i * 2, i * 2 + 1);
 }
 
-int query(node* a,int l,int r,int ql,int qr){
-    //查詢[ql,qr)範圍的最小值
-    if(r<=ql||qr<=l) return inf;
-    if(ql<=l&&r<=qr) return a−>val;
-    int mid=(l+r)/2;
-    return min(query(a−>l,l,mid,ql,qr),
-                query(a−>r,mid,r,ql,qr));
+int query(int ql, int qr, int l, int r, int i) {
+// [ql, qr]是查詢區間,[l, r]是當前節點包含的區間
+    if (ql <= l && r <= qr)
+        return st[i];
+    int mid = l + ((r - l) >> 1);
+    if (tag[i]) {
+        //如果當前懶標有值則更新左右節點
+        st[i * 2] += tag[i] * (mid - l + 1);
+        st[i * 2 + 1] += tag[i] * (r - mid);
+        tag[i * 2] += tag[i];//下傳懶標至左節點
+        tag[i*2+1] += tag[i];//下傳懶標至右節點
+        tag[i] = 0;
+    }
+    int sum = 0;
+    if (ql <= mid) 
+        sum += query(ql, qr, l, mid, i * 2);
+    if (qr > mid)
+        sum += query(ql, qr, mid + 1, r, i*2+1);
+    return sum;
 }
+
+void update(int ql, int qr, int l, int r, int i, int c) {
+// [ql, qr]是查詢區間,[l, r]是當前節點包含的區間,c是變化量
+    if (ql <= l && r <= qr) {
+        st[i] += (r - l + 1) * c; //求和,此需乘上區間長度
+        tag[i] += c;
+        return;
+    }
+    int mid = l + ((r - l) >> 1);
+    if (tag[i] && l != r) {
+        //如果當前懶標有值則更新左右節點
+        st[i * 2] += tag[i] * (mid - l + 1);
+        st[i * 2 + 1] += tag[i] * (r - mid);
+        tag[i * 2] += tag[i];//下傳懶標至左節點
+        tag[i*2+1] += tag[i];//下傳懶標至右節點
+        tag[i] = 0;
+    }
+    if (ql <= mid) update(ql, qr, l, mid, i * 2, c);
+    if (qr > mid) update(ql, qr, mid+1, r, i*2+1, c);
+    st[i] = pull(i * 2, i * 2 + 1);
+}
+//如果是直接改值而不是加值，query與update中的tag與st的
+//改值從+=改成=
