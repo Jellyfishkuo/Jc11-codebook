40363c62b85286331c3db19d2ae6aff5b00e6751
Hao-Hsun-Hsu <samhsu0904@gmail.com>
2024-07-07 20:35:49 +0800

    renamed:    STL/bit.cpp -> DataStructure/BIT.cpp 	modified:   algorithm/ArticulationPoints_Tarjan.cpp 	modified:   content.tex 	modified:   math/atan.cpp

--- algorithm/ArticulationPoints_Tarjan.cpp
+++ algorithm/ArticulationPoints_Tarjan.cpp
@@ -2,7 +2,7 @@ vector<vector<int>> G;
 int N;
 int timer;
 bool visited[105];
-int visTime[105]; // 第一次visit的時間
+int dfn[105]; // 第一次visit的時間
 int low[105]; 
 // 最小能回到的父節點(不能是自己的parent)的visTime
 int res;
@@ -11,23 +11,22 @@ void tarjan(int u, int parent) {
     int child = 0;
     bool isCut = false;
     visited[u] = true;
-    visTime[u] = low[u] = ++timer;
+    dfn[u] = low[u] = ++timer;
     for (int v: G[u]) {
         if (!visited[v]) {
             ++child;
             tarjan(v, u);
             low[u] = min(low[u], low[v]);
-            if (parent != -1 && low[v] >= visTime[u])
+            if (parent != -1 && low[v] >= dfn[u])
                 isCut = true;
         }
         else if (v != parent)
-            low[u] = min(low[u], visTime[v]);
+            low[u] = min(low[u], dfn[v]);
     }
     //If u is root of DFS tree->有兩個以上的children
     if (parent == -1 && child >= 2)
         isCut = true;
-    if (isCut)
-        ++res;
+    if (isCut) ++res;
 }
 int main() {
     char input[105];
@@ -36,7 +35,7 @@ int main() {
         G.assign(105, vector<int>());
         memset(visited, false, sizeof(visited));
         memset(low, 0, sizeof(low));
-        memset(visTime, 0, sizeof(visited));
+        memset(dfn, 0, sizeof(visited));
         timer = 0;
         res = 0;
         getchar(); // for \n
