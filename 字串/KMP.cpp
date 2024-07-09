1dd5dbad0906a2da7db42f2f28845061fc872d50
jimlin2004 <jim.lin2004@gmail.com>
2024-07-09 10:32:32 +0800

    2024/07/09 改3欄後的排版

--- 字串/KMP.cpp
+++ 字串/KMP.cpp
@@ -5,7 +5,8 @@ void getNextArr(const string& str) {
     int prefixLen = 0;
     for (int i = 1; i < str.size(); ++i) {
         prefixLen = nextArr[i - 1];
-        //如果不一樣就在之前算過的prefix中搜有沒有更短的前後綴
+        //如果不一樣就在之前算過的prefix中
+        //搜有沒有更短的前後綴
         while (prefixLen > 0 && str[prefixLen] != str[i])
             prefixLen = nextArr[prefixLen - 1];
         //一樣就繼承之前的前後綴長度+1
