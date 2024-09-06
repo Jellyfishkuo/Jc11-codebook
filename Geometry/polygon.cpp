92aeea0900ba038e3321588fdfe60f920b3fe319
Hao-Hsun-Hsu <113094710+Hao-Hsun-Hsu@users.noreply.github.com>
2024-09-06 13:17:07 +0800

    Update polygon.cpp

--- Geometry/polygon.cpp
+++ Geometry/polygon.cpp
@@ -2,7 +2,7 @@
 bool pointInConvex(Polygon& p, Point point) {
   // 根據 TP 型態來寫，沒浮點數不用 dblcmp
   auto dblcmp=[](DBL v){return (v>0)-(v<0);};
-  // 不包含線上，改 '>=' 為 '<'
+  // 不包含線上，改 '>=' 為 '>'
   auto test = [&](Point& p0, Point& p1) {
     return dblcmp(cross(p1-p0, point-p0))>=0;
   };
