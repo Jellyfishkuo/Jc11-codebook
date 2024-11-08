31cf1c8c3b3ef7f290d8e3e987421809c583ff47
Hao-Hsun-Hsu <samhsu0904@gmail.com>
2024-11-08 14:32:44 +0800

    新增: Suffix Array

--- Geometry/Template/Segment.cpp
+++ Geometry/Template/Segment.cpp
@@ -1,9 +1,9 @@
 struct Segment {
-    Point s, e;
-    Vector v;
-    Segment(): s(0, 0), e(0, 0), v(0, 0) {}
-    Segment(Point s, Point e): s(s), e(e) {
-      v = e - s;
-    }
-    DBL length() { return v.length(); }
+  Point s, e;
+  Vector v;
+  Segment(): s(0, 0), e(0, 0), v(0, 0) {}
+  Segment(Point s, Point e): s(s), e(e) {
+    v = e - s;
+  }
+  DBL length() { return v.length(); }
 };
