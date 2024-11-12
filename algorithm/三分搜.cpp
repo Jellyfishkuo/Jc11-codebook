9d0de9fc059fffce361347d7114e22cf9aede5ec
Jimlin2004 <74666312+jimlin2004@users.noreply.github.com>
2024-11-12 19:01:14 +0800

    修改三分搜的註解

--- algorithm/三分搜.cpp
+++ algorithm/三分搜.cpp
@@ -58,9 +58,9 @@ int main(){
     }
 }
 
-//oi wiki模板
+//oi wiki模板，[l, r]
 //只要是單峰函數，三分可找最大或最小，以下為最小化
-//計算lmid以及rmid十要避免數字overflow
+//計算lmid以及rmid時要避免數字溢出
 while (r - l > eps) {
   mid = (l + r) / 2;
   lmid = mid - eps;
