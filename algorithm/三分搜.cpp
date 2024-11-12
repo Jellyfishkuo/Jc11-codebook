de3cba5ce3fa6129f4a9e817e0ace96b6e439526
Jimlin2004 <74666312+jimlin2004@users.noreply.github.com>
2024-11-12 18:37:18 +0800

    增加二分及三分搜模板，開啟最短路算法

--- algorithm/三分搜.cpp
+++ algorithm/三分搜.cpp
@@ -57,3 +57,14 @@ int main(){
         cout << fixed << setprecision(4) << sqrt(ans) << '\n';
     }
 }
+
+//oi wiki模板
+//只要是單峰函數，三分可找最大或最小，以下為最小化
+//計算lmid以及rmid十要避免數字overflow
+while (r - l > eps) {
+  mid = (l + r) / 2;
+  lmid = mid - eps;
+  rmid = mid + eps;
+  if (f(lmid) < f(rmid)) r = mid;
+  else l = mid;
+}
