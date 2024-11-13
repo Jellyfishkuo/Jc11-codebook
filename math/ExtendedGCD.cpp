4608b42cde65e4b4d4f42e25e4f2113f9a80ee59
Jimlin2004 <74666312+jimlin2004@users.noreply.github.com>
2024-11-13 20:58:54 +0800

    放Extended gcd應用

---
+++ math/ExtendedGCD.cpp
@@ -0,0 +1,64 @@
+題目要求:解 $ax+by=n, a、b \in \mathbb{Z}^{0+}$
+
+已知題幹 $ax+by=n$ 滿足丟番圖方程式\
+同時利用貝祖等式 $ax_1 + by_1 = gcd(a, b)$\
+觀察兩式可知將 $ax_1 + by_1 = gcd(a, b)$ 兩邊乘上 $\frac{n}{gcd(a, b)}$\
+得 $a\frac{nx_1}{gcd(a, b)} + b\frac{ny_1}{gcd(a, b)} = n$\
+此時可看成 $x = \frac{nx_1}{gcd(a, b)}$，$y = \frac{ny_1}{gcd(a, b)}$\
+可以找出一通解
+$$x = \frac{nx_1}{gcd(a, b)} + k \times \frac{b}{gcd(a, b)}$$
+$$y = \frac{ny_1}{gcd(a, b)} - k \times \frac{a}{gcd(a, b)}$$
+$$k \in \mathbb{Z}$$
+(以上通解帶回 $ax + by = n$會發現$k$會被消除)\
+$由於x \geq 0，y \geq 0$
+
+$$\begin{align}
+    所以x = \frac{nx_1}{gcd(a, b)} + k \times \frac{b}{gcd(a, b)} \geq &0 \notag\\
+    y = \frac{ny_1}{gcd(a, b)} - k \times \frac{a}{gcd(a, b)} \geq &0 \notag
+\end{align}$$
+經過移項運算可得
+$$\frac{-nx_1}{b} \leq k \leq \frac{ny_1}{a}$$
+
+ll exgcd(ll a, ll b, ll& x, ll& y) {
+    if (b == 0) {
+        x = 1, y = 0;
+        return a;
+    }
+    ll gcd = exgcd(b, a % b, x, y);
+    ll y1 = y;
+    y = x - (a / b) * y;
+    x = y1;
+    return gcd;
+}
+int main() {
+    ll n;
+    ll x, y;
+    ll c1, c2, a, b;
+    while (~scanf("%lld", &n) && n) {
+        scanf("%lld %lld", &c1, &a);
+        scanf("%lld %lld", &c2, &b);
+        ll gcd = exgcd(a, b, x, y);
+        if (n % gcd != 0) {
+            printf("failed\n");
+            continue;
+        }
+        ll l = ceil((double)(-n) * x / b);
+        ll r = floor((double)(n) * y / a);
+        if (l > r) {
+            printf("failed\n");
+            continue;
+        }
+        if (c1 * b < c2 * a) { //斜率正or負
+            //斜率負，帶入k的上界
+            x = n * x / gcd + b / gcd * r;
+            y = n * y / gcd - a / gcd * r;
+        }
+        else {
+            //斜率正，帶入k的下界
+            x = n * x / gcd + b / gcd * l;
+            y = n * y / gcd - a / gcd * l;
+        }
+        printf("%lld %lld\n", x, y);
+    }
+    return 0;
+}
