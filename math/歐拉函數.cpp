a099757c5793a58f53965e94e702e815348bc9b7
Hao-Hsun-Hsu <113094710+Hao-Hsun-Hsu@users.noreply.github.com>
2025-11-09 15:35:16 +0800

    Enhance phi function with explanatory comments
    Added comments explaining the purpose of the phi function.

--- math/歐拉函數.cpp
+++ math/歐拉函數.cpp
@@ -1,5 +1,5 @@
 //計算閉區間 [1,n] 中有幾個正整數與 n 互質
-
+// a^x = a^(x % phi(M) + phi(M)) mod M
 int phi(){
     int ans=n;
     for(int i=2;i*i<=n;i++)
@@ -9,4 +9,5 @@ int phi(){
         }
     if(n>1) ans=ans-ans/n;
     return ans;
+
 }
